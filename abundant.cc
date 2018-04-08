9fdc38f930dfea961757f224e866e3e9d43c8119
Lari Alakukku <lari.alakukku@aalto.fi>
2018-04-08 13:48:02 +0300

    my solution

---
+++ abundant.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <vector>
+
+//These should be in their own file.
+
+bool is_an_abundant_number(long n)
+{
+  long sum = 1;
+  //We can continue looping untill sqrt(n)
+  //Every found divisior is part of a pair expect a sqrt(n) that is a divisor.
+  for (long i = 2; i <= n / i; ++i)
+  {
+
+    if ((n / i) == i) {
+        sum += i;
+        break;
+    }
+
+    if (n % i == 0) {
+      sum += 1;
+      sum += n/i;
+    }
+
+  }
+  return n < sum;
+}
+
+std::vector< long > find_first_n_abundants(long n)
+{
+  long counter = 0;
+  std::vector< long > result;
+
+  for (long i = 1; counter < n; ++i)
+  {
+    if (is_an_abundant_number(i)) {
+        result.push_back(i);
+        ++counter;
+    }
+  }
+
+  return result;
+}
+
+void print_vector(std::vector< long > v)
+{
+  for (auto b = v.begin(); b != v.end(); ++b)
+  {
+    std::cout << *b << std::endl;
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  if (argc < 2) {
+    std::cout << "Too few arguments!" << std::endl;
+    return 1;
+  }
+  else {
+
+    long n = std::stol(argv[1]);
+
+    auto v = find_first_n_abundants(n);
+
+    print_vector(v);
+
+  }
+
+  return 0;
+}
